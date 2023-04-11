d7d6ba10a5cb8bdcf5e2e698897a161488e87970
Jack Latrobe <jack@jack-latrobe.com>
2023-04-11 19:45:55 +1000

    Changed backend to local

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,13 +1,8 @@
 # HelpChain-Infra: backend.tf
 
 terraform {
-  backend "s3" {
-    bucket         = "<your-s3-bucket-name>"
-    key            = "<your-state-file-key>"
-    region         = "<aws-region>"
-    access_key     = "<your-aws-access-key>"
-    secret_key     = "<your-aws-secret-key>"
-    dynamodb_table = "<your-lock-table-name>"
-    encrypt        = true
+  backend "local" {
+    path = "helpchain-infra.tfstate"
   }
 }
+
