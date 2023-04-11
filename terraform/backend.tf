e1fd6874e4676f7d4d7bee960c3e14fb04be02f0
Jack Latrobe <jack@jack-latrobe.com>
2023-04-11 20:03:15 +1000

    Fleshed out main to baseline

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,19 @@
 # HelpChain-Infra: backend.tf
 
+# Configure the Terraform backend
 terraform {
   backend "local" {
     path = "helpchain-infra.tfstate"
   }
+  required_providers {
+      digitalocean = {
+        source = "digitalocean/digitalocean"
+        version = "~> 2.0"
+      }
+  }
 }
 
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.do_token
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,38 @@
 # HelpChain-Infra: main.tf
 
 ## Digital Ocean Project: To organize and manage your resources within the Digital Ocean platform.
+## https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs/resources/project
 resource "digitalocean_project" "helpchain_project" {
   name        = var.project_name
   description = "HelpChain chatbot application"
 }
 
+
+## Digitial Ocean Virtual Private Network: To establish a secure network perimetre for the solution
+resource "digitalocean_vpc" "helpchain_vpc" {
+  name        = "helpchain_vpc"
+  description = "Default network for the helpchain GPT solution"
+  region      = var.region
+  ip_range    = "10.10.10.0/24"
+}
+
+
 ## Digital Ocean Kubernetes Cluster: A managed Kubernetes cluster that will host your application.
+## https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs/resources/kubernetes_cluster
+## https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs/resources/kubernetes_node_pool
 resource "digitalocean_kubernetes_cluster" "helpchain_cluster" {
-  name         = "${var.project_name}-cluster"
-  region       = var.region
-  version      = var.kubernetes_version
-  project_id   = digitalocean_project.helpchain_project.id
+  name          = "${var.project_name}-cluster"
+  region        = var.region
+  version       = var.kubernetes_version
+  auto_upgrade  = true
+  surge_upgrade = true
+  vpc_uuid      = digitalocean_vpc.helpchain_vpc.id
+  project_id    = digitalocean_project.helpchain_project.id
+
+  maintenance_policy {
+    start_time  = "02:00"
+    day         = "sunday"
+  }
 
   node_pool {
     name       = "worker-pool"
@@ -19,5 +40,3 @@ resource "digitalocean_kubernetes_cluster" "helpchain_cluster" {
     node_count = var.worker_node_count
   }
 }
-
-## Digital Ocean Kubernetes Node Pool: A group of worker nodes that will run your application containers.
