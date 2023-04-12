84adeef91722816b32705ac3b82850879dbd989c
Jack Latrobe <jack@jack-latrobe.com>
2023-04-12 10:14:04 +1000

    Moved state file to do spaces

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,14 +2,20 @@
 
 # Configure the Terraform backend
 terraform {
-  backend "local" {
-    path = "helpchain-infra.tfstate"
+  backend "s3" {
+    key = "terraform.state"
+    bucket = "helpchain-state"
+    region = "us-east-1"
+    endpoint = "https://nyc3.digitaloceanspaces.com"
+    skip_credentials_validation = true
+    skip_region_validation = true
+    skip_metadata_api_check = true
   }
   required_providers {
-      digitalocean = {
-        source = "digitalocean/digitalocean"
-        version = "~> 2.0"
-      }
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
   }
 }
 
